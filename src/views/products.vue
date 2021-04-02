ed2dd24b077c5bbe38602ea3335c72b8b5d316c3
david <1982043795@qq.com>
2021-04-02 13:57:54 +0800

    初始化demo

---
+++ src/views/products.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item><a href="#/">首页</a></el-breadcrumb-item>
+      <el-breadcrumb-item><a href="#/">商品列表</a></el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-table
+      :data="products"
+      style="width: 100%">
+      <el-table-column
+        prop="title"
+        label="商品">
+      </el-table-column>
+      <el-table-column
+        prop="price"
+        label="价格">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+
+        label="操作">
+        <template v-slot='{row}'>
+          <el-button @click="addToCart(row)" >加入购物车</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+<script>
+import { mapActions, mapMutations, mapState } from 'vuex'
+export default {
+  name: 'ProductList',
+  data () {
+    return {
+
+    }
+  },
+  created () {
+    this.getProducts()
+  },
+  computed: {
+    ...mapState('products', ['products'])
+  },
+  methods: {
+    ...mapActions('products', ['getProducts']),
+    ...mapMutations('cart',['addToCart'])
+  }
+}
+</script>
+
+<style></style>
